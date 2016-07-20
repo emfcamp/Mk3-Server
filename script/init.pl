64fe802ec4d0012b08a1c7d41aa7a5d73c69592a
Tom Bloor <tom.bloor@googlemail.com>
2016-07-20 22:07:20 +0100

    Initial startup script work

---
+++ script/init.pl
@@ -0,0 +1,25 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Daemon::Control;
+use FindBin qw/ $Bin /;
+
+exit Daemon::Control->new(
+    name        => 'MK3 AppServer',
+    lsb_start   => '$syslog $remote_fs',
+    lsb_stop    => '$syslog',
+    lsb_sdesc   => 'MK3 AppServer init',
+    lsb_desc    => 'Init script for the MK3 Appserver fo EMF Camp',
+ 
+    program     => "$Bin/mk3_appserver_server.pl",
+    program_args => [ qw/ -p 5000 / ],
+ 
+    pid_file    => "$Bin/../../mk3appserver.pid",
+    stderr_file => "$Bin/../../mk3appserver.err",
+    stdout_file => "$Bin/../../mk3appserver.out",
+ 
+    fork        => 2,
+ 
+)->run;
